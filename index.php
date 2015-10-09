84d35419ac4ab9a02cf095f04e0ec9065a9b77e6
kujirahand <web@kujirahand.com>
2015-10-09 16:23:47 +0900

    add index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+// set config
+define("KONA3_WIKI_TITLE", "Konawiki3");
+define("KONA3_WIKI_USERS", "kona3:pass3,kona2:pass2");
+
+// Include engine/index.inc.php
+include "kona3engine/index.inc.php";
+
