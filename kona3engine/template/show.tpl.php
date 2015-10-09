27ef18a8b1e8e64eadd8c8e83d6c699f08977977
kujirahand <web@kujirahand.com>
2015-10-09 20:59:27 +0900

    modified menu layout.

--- kona3engine/template/show.tpl.php
+++ kona3engine/template/show.tpl.php
@@ -5,6 +5,7 @@ global $kona3conf;
 $wikibody = <<<EOS
 <div id="wikibody">{$page_body}</div>
 <div id="wikimenu"><nav>{$wiki_menu}</nav></div>
+<div style="clear:both;"></div>
 EOS;
 
 // $page_title $page_body $wiki_menu
