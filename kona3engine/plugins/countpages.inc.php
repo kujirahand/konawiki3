40897b0bb437678150a9426f7f342cbbd2d02192
kujirahand <web@kujirahand.com>
2015-12-24 18:49:31 +0900

    Update plugin countpages.inc.php read recursive

--- kona3engine/plugins/countpages.inc.php
+++ kona3engine/plugins/countpages.inc.php
@@ -5,12 +5,15 @@ function kona3plugins_countpages_execute($args) {
   $path = $kona3conf["path.data"];
   $cnt_txt = 0;
   $cnt_src = 0;
-  $files = glob($path."/*.txt");
+  $cnt_file = 0;
+  $cnt_code = 0;
+  $files = enum_files($path, "#\.txt$#");
   //
   foreach($files as $f) {
     $txt = @file_get_contents($f);
     $c = mb_strlen($txt);
     $cnt_txt += $c;
+    $cnt_file++;
     
     // search #filecode(***)
     $lines = explode("\n", $txt);
@@ -21,6 +24,7 @@ function kona3plugins_countpages_execute($args) {
         $subtxt = @file_get_contents($src);
         $c = mb_strlen($subtxt);
         $cnt_src += $c;
+        $cnt_code++;
       }
     }
   }
@@ -34,8 +38,34 @@ function kona3plugins_countpages_execute($args) {
   $cnt_txt_f = number_format($cnt_txt);
   $page_txt = floor($cnt_txt / 1000);
   return
-    "<span>合計 {$cnt_f}字, {$page_f}ページ".
-    "&nbsp;(テキストのみ:{$cnt_txt_f}字, {$page_txt}ページ)</span>";
+    "<span>".
+    "<b>合計 {$cnt_f}字</b>, {$page_f}ページ".
+    "&nbsp;(テキストのみ:{$cnt_txt_f}字, {$page_txt}ページ)".
+    "&nbsp;ファイル数:{$cnt_file}, ソース数: {$cnt_code}".
+    "</span>";
 }
+
+function enum_files($dir, $pat = NULL) {
+  $files = scandir($dir);
+  $dir = rtrim($dir, '/'); // chomp '/'
+  $list = array();
+  foreach ($files as $file) {
+    if ($file == "." || $file == "..") continue;
+    $fullpath = $dir . '/' . $file;
+    if (is_file($fullpath)) {
+      if ($pat !== NULL) {
+        if (!preg_match($pat, $file)) continue;
+      }
+      $list[] = $fullpath;
+    }
+    if (is_dir($fullpath)) {
+      $list = array_merge($list, enum_files($fullpath, $pat));
+    }
+  }
+  return $list;
+}
+
+
+
 
 
