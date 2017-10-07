634d48f79081b0d1f542a93d52ad5666ac5cd0ba
kujirahand <web@kujirahand.com>
2017-10-07 13:46:48 +0900

    index.php will check KONA3_DIR_ENGINE path

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ if (file_exists($config_file)) {
   define("KONA3_WIKI_TITLE", "Konawiki3");
   define("KONA3_WIKI_USERS", "kona3:pass3:3,kona2:pass2"); # admin users
   define("KONA3_WIKI_PRIVATE", true); # true or false
+  define("KONA3_DIR_ENGINE", dirname(__FILE__).'/kona3engine');
   define("KONA3_DIR_DATA", dirname(__FILE__).'/data');
   define("KONA3_DIR_PRIVATE", dirname(__FILE__).'/private');
   define("KONA3_DSN", "sqlite:".KONA3_DIR_PRIVATE."/data.sqlite");
@@ -20,4 +21,9 @@ if (file_exists($config_file)) {
 }
 
 // Include kona3engine/index.inc.php
-include "kona3engine/index.inc.php";
+$engine_index = "kona3engine/index.inc.php";
+if (defined("KONA3_DIR_ENGINE")) {
+  $engine_index = KONA3_DIR_ENGINE."/index.inc.php";
+}
+require_once($engine_index);
+
