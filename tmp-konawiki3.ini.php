c5ec27e4c71755512a8601015a15945682571bde
kujirahand <kujira@kujirahand.com>
2020-11-19 17:49:35 +0900

    add Git setting to tmp-konawiki3.ini.php

--- tmp-konawiki3.ini.php
+++ tmp-konawiki3.ini.php
@@ -8,8 +8,8 @@ define("KONA3_WIKI_TITLE", "Konawiki3");
 define("KONA3_WIKI_PRIVATE", TRUE); # TRUE or FALSE
 define("KONA3_WIKI_SKIN", "def"); // def/single
 
-// Admin Users "user1:password1,user2:password2"
-define("KONA3_WIKI_USERS", "##WIKI_USERS##");
+// Wiki User info
+define("KONA3_WIKI_USERS", "##WIKI_USERS##"); // Admin Users
 define("KONA3_PASSWORD_SALT", "##WIKI_SALT##"); // Please modify before add users
 define("KONA3_ADMIN_EMAIL", ""); // Please set your email
 define("KONA3_ALLOW_ADD_USER", FALSE);
@@ -20,6 +20,7 @@ define("KONA3_DIR_ENGINE", __DIR__.'/kona3engine');
 define("KONA3_DIR_DATA", __DIR__.'/data');
 define("KONA3_DIR_PRIVATE", __DIR__.'/private');
 define("KONA3_DIR_CACHE", __DIR__.'/cache');
+
 // Files
 define("KONA3_DSN", "sqlite:".KONA3_DIR_PRIVATE."/data.sqlite");
 
@@ -28,5 +29,6 @@ define("KONA3_SHOW_DATA_DIR", FALSE); // show local path
 define("KONA3_ALLPAGE_HEADER", ""); 
 define("KONA3_ALLPAGE_FOOTER", "#comment"); // footer
 define("KONA3_ENC_PAGENAME", FALSE); // 日本語名をURLエンコードするか
+define("KONA3_GIT_ENABLED", FALSE); # Git Support
 
 
