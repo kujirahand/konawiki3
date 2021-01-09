10696c97006bc12420f2d7d66303dc88aacd4a21
kujirahand <kujira@kujirahand.com>
2021-01-09 18:58:26 +0900

    nako3 plugins v3.1.12

--- kona3engine/fw_template_engine_plugins.lib.php
+++ kona3engine/fw_template_engine_plugins.lib.php
@@ -3,58 +3,104 @@
 // escape value
 function t_echo($v) {
   $v = htmlspecialchars($v);
-  echo $v;
+  return $v;
 }
 function t_multiline($v) {
   $v = htmlspecialchars($v);
   $v = preg_replace('#(\r\n|\n|\r)#s', '<br>', $v);
-  echo $v;
+  return $v;
+}
+function t_check_mudai($v) {
+  if (empty($v)) { $v = '(無題)'; }
+  $v = trim_url($v);
+  $v = mb_strimwidth($v, 0, 100, ' ... ');
+  return t_echo($v);
+}
+function t_check_nanasi($v) {
+  if (empty($v)) { $v = '名無し'; }
+  $v = trim_url($v);
+  $v = mb_strimwidth($v, 0, 100, ' ... ');
+  return t_echo($v);
+}
+function t_trim100($v) {
+  if (empty($v)) { $v = '(なし)'; }
+  $v = trim_url($v);
+  $v = mb_strimwidth($v, 0, 100, ' ... ');
+  return t_echo($v);
+}
+function trim_url($url) {
+  $url = preg_replace_callback('#([a-zA-Z0-9_\-\/\:\.]{11,})#', function($m) {
+    $s = $m[1];
+    $s = preg_replace('#(http://|https://)#', '', $s);
+    return substr($s, 0, 10).' ... ';
+  }, $url);
+  return $url;
 }
 // raw
 function t_raw($v) {
-  echo $v;
+  return $v;
 }
 function t_safe($v) {
-  echo $v;
+  return $v;
 }
+
+function t_star($v) {
+  $mi = $ni = intval($v);
+  if ($mi > 3) {
+    $mi = 5;
+  }
+  $s = "";
+  for ($i = 0; $i < $mi; $i++) {
+    $s .= '⭐';
+  }
+  if ($ni > 5) {
+    $s = "<span class='crown'>{$s}...👍".$ni."</span>";
+  }
+  return $s;
+}
+
 // date
 function t_date($v) {
-  echo date('Y年m月d日', $v);
+  return date('Y年m月d日', $v);
+}
+function t_date2($v) {
+  return date('Y-m-d', $v);
 }
 function t_datetime($v) {
-  echo date('Y/m/d H:i', $v);
+  return date('Y/m/d H:i', $v);
 }
 // format
 function t_number_format($v) {
   $v = floatval($v);
-  echo number_format($v);
+  return number_format($v);
 }
 // lang
 function t_lang($msg) {
-  echo lang($msg);
+  return lang($msg);
 }
 // boolstr
 function t_boolstr($v) {
   if ($v) {
-    echo "true";
+    return "true";
   } else {
-    echo "false";
+    return "false";
   }
 }
 
-
 // --- {{{e:xxx}}} ---
 function echo_options($sel_value, $labels, $values) {
+  $ret = '';
   $cnt = count($labels);
   for ($i = 0; $i < $cnt; $i++) {
-    $label = $labels[$i];
-    $value = $values[$i];
+    $label = htmlspecialchars($labels[$i]);
+    $value = htmlspecialchars($values[$i]);
     $selected = '';
     if ($value == $sel_value) {
       $selected = ' selected';
     }
-    echo "<option value=\"$value\"$selected>$label</option>\n";
+    $ret .= "<option value=\"$value\"$selected>$label</option>\n";
   }
+  return $ret;
 }
 
 
