1426542484d7b3b9259ffab57ef04f23ed0e15a6
kujirahand <kujira@kujirahand.com>
2021-04-09 21:07:15 +0900

    filecode plugin になでしこ3の拡張を追加

--- kona3engine/plugins/filecode.inc.php
+++ kona3engine/plugins/filecode.inc.php
@@ -1,7 +1,11 @@
 <?php
+/**
+ * #filecode(path)
+ */
 
 function kona3plugins_filecode_execute($args) {
   global $kona3conf;
+  // get file path
   $name = array_shift($args);
   $name = str_replace('..', '', $name);
   $fname = kona3getWikiFile($name, false);
@@ -18,20 +22,31 @@ function kona3plugins_filecode_execute($args) {
   }
   $url = kona3getWikiUrl($name);
   $txt = @file_get_contents($fname);
+  $name_ = htmlspecialchars($name, ENT_QUOTES);
   if (preg_match('#\.php$#', $fname)) {
     $txt = trim($txt);
     $htm = highlight_string($txt, true);
     // <pre>するので不必要な改行を削除
     $htm = preg_replace('#[\r\n]#', '', $htm);
+  } else if (preg_match('#\.(nako|nako3)$#', $fname)) {
+    // #nako3 plugin
+    $txt = str_replace('{{{', ' {{{', $txt); // escape
+    $txt = str_replace('}}}', ' }}}', $txt);
+    $cnt = substr_count($txt, "\n");
+    $head = "<div class='filecode'>".
+            "  <div class='filename'>file: {$name_}</div>".
+            "</div>";
+    $code  = "{{{#nako3(canvas,rows=$cnt)\n";
+    $code .= trim($txt) . "\n";
+    $code .= '}}}'."\n";
+    $htm = $head . konawiki_parser_convert($code);
+    return $htm;
   } else {
     $htm = kona3text2html(trim($txt));
   }
-  $name_ = htmlspecialchars($name, ENT_QUOTES);
   $code =
     "<div class='filecode'>".
-    "  <div class='filename'>".
-    "    <a href='$url'>file: {$name_}</a>".
-    "  </div>".
+    "  <div class='filename'>file: {$name_}</div>".
     "  <pre class='code'>$htm</pre>".
     "</div>";
   return $code;
