167f943820075769b54e6242af162709aa55c138
kujirahand <web@kujirahand.com>
2023-04-08 21:35:04 +0900

    updated nako3 ver.3.4.17

--- kona3engine/plugins/csv.inc.php
+++ kona3engine/plugins/csv.inc.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * - [書式] {{{#csv([noheader][flag=xxx][cell=xxx]) データ }}}
  * - [引数]
@@ -7,66 +8,68 @@
  * -- cell=xxx ... セルの変換(wiki|text) wikiでwiki記法をパース/デフォルトはwiki
  * -- plain ... 強制セルの変換を行わない
  * -- データ ... CSVデータを指定する
-*/
-function kona3plugins_csv_execute($params) {
-    if (!$params) return "";
-    $cellType = "wiki";
-    $noheader = FALSE;
-    $csv = "";
-    $delimiter = ",";
-    foreach ($params as $s) {
-        if ($s == "noheader") {
-            $noheader = TRUE;
-            continue;
-        }
-        if ($s == 'plain') {
-            $cellType = 'text';
-            continue;
-        }
-        if (preg_match('#flag\=(.+)#', $s, $m)) {
-          $delimiter = $m[1];
-          continue;
-        }
-        if (preg_match('#wiki\=(.+)#', $s, $m)) {
-          $cellType = $m[1];
-          continue;
-        }
-        $csv = $s;
-        break;
+ */
+function kona3plugins_csv_execute($params)
+{
+  if (!$params) return "";
+  $cellType = "wiki";
+  $noheader = FALSE;
+  $csv = "";
+  $delimiter = ",";
+  foreach ($params as $s) {
+    if ($s == "noheader") {
+      $noheader = TRUE;
+      continue;
     }
-    
-    $html = "<table>\n";
-    $lines = explode("\n", trim($csv));
-    // header
-    if ($noheader == FALSE) {
-        $line = array_shift($lines);
-        $cols = explode($delimiter, $line);
-        $html .= "<tr>";
-        foreach ($cols as $col) {
-            $col = trim($col);
-            $col = _cell($col, $cellType);
-            $html .= "<th>{$col}</th>";
-        }
-        $html .= "</tr>\n";
+    if ($s == 'plain') {
+      $cellType = 'text';
+      continue;
     }
-    // csv body
-    foreach ($lines as $line) {
-        $line = trim($line);
-        if (!$line) continue;
-        $cols = explode($delimiter, $line);
-        $html .= "<tr>";
-        foreach ($cols as $col) {
-            $col = trim($col);
-            $col = _cell($col, $cellType);
-            $html .= "<td>$col</td>";
-        }
-        $html .= "</tr>\n";
+    if (preg_match('#flag\=(.+)#', $s, $m)) {
+      $delimiter = $m[1];
+      continue;
     }
-    $html .= "</table>\n";
-    return $html;
+    if (preg_match('#wiki\=(.+)#', $s, $m)) {
+      $cellType = $m[1];
+      continue;
+    }
+    $csv = $s;
+    break;
+  }
+
+  $html = "<table>\n";
+  $lines = explode("\n", trim($csv));
+  // header
+  if ($noheader == FALSE) {
+    $line = array_shift($lines);
+    $cols = explode($delimiter, $line);
+    $html .= "<tr>";
+    foreach ($cols as $col) {
+      $col = trim($col);
+      $col = _cell($col, $cellType);
+      $html .= "<th>{$col}</th>";
+    }
+    $html .= "</tr>\n";
+  }
+  // csv body
+  foreach ($lines as $line) {
+    $line = trim($line);
+    if (!$line) continue;
+    $cols = explode($delimiter, $line);
+    $html .= "<tr>";
+    foreach ($cols as $col) {
+      $col = trim($col);
+      $col = _cell($col, $cellType);
+      $html .= "<td>$col</td>";
+    }
+    $html .= "</tr>\n";
+  }
+  $html .= "</table>\n";
+  return $html;
 }
 
-function _cell($cell, $type) {
+function _cell($cell, $type)
+{
   if ($type == 'wiki') {
     $cell = konawiki_parser_convert($cell, FALSE);
     return $cell;
@@ -74,6 +77,3 @@ function _cell($cell, $type) {
   $cell = kona3text2html($cell);
   return $cell;
 }
-
-
-
--- kona3engine/plugins/nako3/nako_version.inc.php
+++ kona3engine/plugins/nako3/nako_version.inc.php
@@ -1,3 +1,3 @@
 <?php
-define('NAKO_DEFAULT_VERSION', '3.4.14');
+define('NAKO_DEFAULT_VERSION', '3.4.17');
 
