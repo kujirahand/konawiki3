ddaad6316083423a51758a7662d37191bb537385
kujirahand <web@kujirahand.com>
2023-08-27 23:52:37 +0900

    add markdown plugin

---
+++ kona3engine/plugins/markdown.inc.php
@@ -0,0 +1,11 @@
+<?php
+include_once dirname(__DIR__).'/kona3parser_md.inc.php';
+// {{{#markdown ... }}} の書式で使う
+// [options]
+//   pre ... <pre>結果をタグで囲む
+//   wiki ... Wiki記法でレンダリングする
+function kona3plugins_markdown_execute($args) {
+  $text = array_pop($args);
+  $html = kona3markdown_parser_convert($text);
+  return "<div class='beta'>{$html}</div>";
+}
