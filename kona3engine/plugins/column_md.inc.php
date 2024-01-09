ee5f76f9036cbd83fb76c28d62a390d98e23800e
kujirahand <web@kujirahand.com>
2024-01-09 14:38:03 +0900

    add #column_md plugin

---
+++ kona3engine/plugins/column_md.inc.php
@@ -0,0 +1,22 @@
+<?php
+
+/** コラムの囲み
+ * - [書式] {{{#column(class) ... }}}
+ * - [引数]
+ * -- class ... クラス名(省略可)
+ * - [公開設定] 公開
+ */
+
+function kona3plugins_column_md_execute($args) {
+  if (!$args) return "";
+  $class = "column";
+  if (count($args) >= 2) {
+      $class = trim(array_shift($args));
+  }
+  $body  = array_shift($args);
+  if (!preg_match("/^([0-9a-zA-Z\-\_]+)$/",$class)) {
+      $class = "column";
+  }
+  $html = kona3markdown_parser_convert($body, FALSE);
+  return "<div class='$class'>{$html}</div>";
+}
