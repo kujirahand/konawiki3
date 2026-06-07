2d5900d324fe9e68620bcbb67c0012df55c67073
kujirahand <web@kujirahand.com>
2026-06-07 10:54:38 +0900

    fix #177

--- kona3engine/tests/page_id.test.php
+++ kona3engine/tests/page_id.test.php
@@ -2,5 +2,13 @@
 require_once __DIR__ . '/test_common.inc.php';
 
 // page_id test
-test_eq(__LINE__, kona3db_getPageId("FrontPage", TRUE), 1, "kona3db_getPageId ... Please setup wiki");
-test_eq(__LINE__, kona3db_getPageNameById(1), "FrontPage", "kona3db_getPageNameById");
+$page = "PageIdTest_" . time();
+$page_id = kona3db_getPageId($page, TRUE);
+test_assert(__LINE__, is_numeric($page_id) && $page_id > 0, "kona3db_getPageId creates a page ID");
+test_eq(__LINE__, kona3db_getPageId($page, FALSE), $page_id, "kona3db_getPageId returns the same ID for an existing page");
+test_eq(__LINE__, kona3db_getPageNameById($page_id), $page, "kona3db_getPageNameById returns the created page name");
+
+$cached_page = "PageIdCacheTest_" . time();
+$cached_id = kona3db_getPageId($cached_page, TRUE);
+test_assert(__LINE__, is_numeric($cached_id) && $cached_id > 0, "kona3db_getPageId creates a page ID after reverse lookup cache exists");
+test_eq(__LINE__, kona3db_getPageNameById($cached_id), $cached_page, "kona3db_getPageNameById sees newly created IDs after cache invalidation");
